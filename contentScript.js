022b5420f388dbdc42a88e78a6d37af6011f8f30
Brian Norman <briannorman9@gmail.com>
2021-08-24 15:39:06 -0600

    updates

--- contentScript.js
+++ contentScript.js
@@ -1,60 +1,48 @@
-const waitForElement = async selector => {
-    while (document.querySelector(selector) === null) {
-        await new Promise(resolve => requestAnimationFrame(resolve))
-    }
-    return document.querySelector(selector);
-};
-
-/**
- * Get the environmentID and set it in Chrome storage
- */
-waitForElement('script[src*="participants.evolv.ai/v1/"]').then(el => {
-    let src = el.src;
-    let v1Index = src.indexOf('v1/');
-    let envID = src.substr(v1Index + 3);
-    let slashIndex = envID.indexOf('/');
-    envID = envID.substr(0, slashIndex);
-
-    chrome.storage.sync.set({ "envID": envID });
+// window event is triggered in tampermonkey.js indicating that our extension is ready to rock'n'roll
+window.addEventListener('run_content_script', function() {
+    const waitForElement = async selector => {
+        while (document.querySelector(selector) === null) {
+            await new Promise(resolve => requestAnimationFrame(resolve))
+        }
+        return document.querySelector(selector);
+    };
+    
+    /**
+     * Get the environmentID and set it in Chrome storage
+     */
+    waitForElement('script[src*="participants.evolv.ai/v1/"]').then(el => {
+        let src = el.src;
+        let v1Index = src.indexOf('v1/');
+        let envID = src.substr(v1Index + 3);
+        let slashIndex = envID.indexOf('/');
+        envID = envID.substr(0, slashIndex);
+        chrome.storage.sync.set({ "envID": envID });
+    });
+    
+    /**
+     * Set the User ID in Chrome storage
+     */
+    let uid = window.localStorage.getItem('evolv:uid');
+    chrome.storage.sync.set({ "evolv:uid": uid });
+    
+    /**
+     * Set the Session ID in Chrome storage
+     */
+    let sid = window.sessionStorage.getItem('evolv:sid');
+    chrome.storage.sync.set({ "evolv:sid": sid });
+    
+    /**
+     * Grab the allocations from sessionStorage and set it in chrome storage.
+     * The allocations are set by the Tampermonkey script.
+     */
+    
+    let allocations = JSON.parse(window.sessionStorage.getItem('evolv:allocations'))
+    chrome.storage.sync.set({ "evolv:allocations": allocations });
+    chrome.runtime.sendMessage({message: "allocations_updated"});
+    
+    /**
+     * Set the Candidate Token in Chrome storage
+     */
+     let candidateToken = window.sessionStorage.getItem('evolv:candidateToken');
+     chrome.storage.sync.set({ "evolv:candidateToken": candidateToken });
 });
-
-/**
- * Set the User ID in Chrome storage
- */
-let uid = window.localStorage.getItem('evolv:uid');
-chrome.storage.sync.set({ "evolv:uid": uid });
-
-/**
- * Set the Session ID in Chrome storage
- */
-let sid = window.sessionStorage.getItem('evolv:sid');
-chrome.storage.sync.set({ "evolv:sid": sid });
-
-/**
- * Set the Candidate Token in Chrome storage
- */
-let candidateToken = window.sessionStorage.getItem('evolv:candidateToken');
-chrome.storage.sync.set({ "evolv:candidateToken": candidateToken });
-
-/**
- * Grab the allocations from sessionStorage and set it in chrome storage.
- * The allocations are set by the Tampermonkey script.
- */
-let allocations = JSON.parse(window.sessionStorage.getItem('evolv:allocations'))
-chrome.storage.sync.set({ "evolv:allocations": allocations });
-
-/**
- * Listen for messages coming from the extension
- */
-// chrome.runtime.onMessage.addListener(
-//     function (request, sender, sendResponse) {
-//         if (request.action === 'update_values') {
-//             console.log('hey brian request', request)
-//             window.localStorage.setItem('evolv:uid', request.content.uid);
-//             window.sessionStorage.setItem('evolv:sid', request.content.sid);
-//             window.sessionStorage.setItem('evolv:candidateToken', request.content.candidateToken);
-
-//             sendResponse("values updated");
-//         }
-//     }
-// );
--- tampermonkey.js
+++ tampermonkey.js
@@ -10,20 +10,61 @@
 // ==/UserScript==
 
 ;(function () {
-  "use strict";
+  "use strict"
 
-  const waitForElement = async (selector) => {
-    while (document.querySelector(selector) === null) {
-      await new Promise((resolve) => requestAnimationFrame(resolve));
-    }
-    return document.querySelector(selector);
-  };
+  function run() {
+    let confirmedAllocations = [];
+    (function poll() {
+      if (!(window.evolv &&
+          window.evolv.context &&
+          window.evolv.context.remoteContext &&
+          window.evolv.context.remoteContext.confirmations)) {
+        return setTimeout(poll, 50);
+      }
 
-  // set the allocations in localStorage
-  waitForElement('script[src*="participants.evolv.ai/v1/"]').then((el) => {
-    let allocations =
-      window.evolv.client.context.remoteContext.experiments.allocations;
-    let allocationsJSON = JSON.stringify(allocations);
-    window.sessionStorage.setItem("evolv:allocations", allocationsJSON);
+      let allocations = window.evolv.context.remoteContext.experiments.allocations;
+      let confirmations = window.evolv.context.remoteContext.confirmations;
+
+      for (let i = 0; i < allocations.length; i++) {
+        let allocation = allocations[i];
+        let allocationCID = allocation.cid;
+
+        for (let j = 0; j < confirmations.length; j++) {
+          let confirmationCID = confirmations[j].cid;
+          if (allocationCID === confirmationCID) {
+            confirmedAllocations.push(allocation);
+          }
+        }
+      }
+
+      let allocationsJSON = JSON.stringify(confirmedAllocations);
+      window.sessionStorage.setItem("evolv:allocations", allocationsJSON);
+      window.dispatchEvent(new Event('run_content_script'));
+    })();
+  }
+
+  /**
+   * Handle SPA transitions
+   */
+  window.addEventListener('locationchange', function () {
+    run();
+  });
+
+  history.pushState = (f => function pushState() {
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('pushState'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  })(history.pushState);
+
+  history.replaceState = (f => function replaceState() {
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('replaceState'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  })(history.replaceState);
+
+  window.addEventListener('popstate', () => {
+    window.dispatchEvent(new Event('locationchange'))
   });
 })();
